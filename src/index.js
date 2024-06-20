7745e3a02197ce680ae94cc96058cc03c9621290
alex241728 <alex_241728@163.com>
2024-06-20 14:32:39 -0400

    default

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+    return (
+        <div className="card">
+            <Avatar />
+            <div className="data">
+                <Intro />
+                <MoePointList />
+            </div>
+        </div>
+    );
+}
+
+function Avatar() {
+    return <img className="avatar" src={require("./images/firefly.jpg")} alt="Firefly"/>
+}
+
+function Intro() {
+    return (
+        <div className="intro">
+            <h1>Firefly(AR-26710)</h1>
+            <p>
+                A member of the Stellaron Hunters and a young girl clad in a mechanical armor "SAM." Born as a weapon,
+                she is afflicted with the agony of Entropy Loss Syndrome due to genetic modification. She joined the
+                Stellaron Hunters in search of the meaning of life, relentlessly pursuing ways to defy fate.
+            </p>
+        </div>
+    );
+}
+
+function MoePointList() {
+    return (
+        <div className="moe-point-list">
+            <MoePoint moepoint="strong mecha" emoji="💪" color="green"/>
+            <MoePoint moepoint="tender" color="orangered"/>
+            <MoePoint moepoint="caefly" emoji="❤️" color="yellow" />
+        </div>
+    );
+}
+
+function MoePoint(props) {
+    return (
+        <div className="moe-point" style={
+            {
+                backgroundColor: props.color
+            }
+        }>
+            <span>{props.moepoint}</span>
+            <span>{props.emoji}</span>
+        </div>
+    );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
